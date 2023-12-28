952cb49a2544077b0c1c631a7e86eafbea763cc8
German Renato Cobian <germancobian@hotmail.com>
2023-12-28 12:50:23 -0800

    Create login and logout methods for authentication actions

--- src/app/actions/auth.js
+++ src/app/actions/auth.js
@@ -34,3 +34,41 @@ export const signupUser = (credentials) => (dispatch) => fetch('http://localhost
       return Promise.reject(errors);
     });
 });
+
+export const loginUser = (credentials) => (dispatch) => fetch('http://localhost:3001/login', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ user: credentials }),
+}).then((res) => {
+  if (res.ok) {
+    setToken(res.headers.get('Authorization'));
+    return res
+      .json()
+      .then((userJson) =>
+        dispatch({ type: AUTHENTICATED, payload: userJson }));
+  }
+    return res.json().then((errors) => {
+      dispatch({ type: NOT_AUTHENTICATED });
+      return Promise.reject(errors);
+    });
+});
+
+export const logoutUser = () => (dispatch) => fetch('http://localhost:3001/logout', {
+  method: 'DELETE',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: getToken(),
+  },
+}).then((res) => {
+  if (res.ok) {
+    return dispatch({ type: NOT_AUTHENTICATED });
+  }
+    return res.json().then((errors) => {
+      dispatch({ type: NOT_AUTHENTICATED });
+      return Promise.reject(errors);
+    });
+});
